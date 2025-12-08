9c88d088fcfa5bd2f59cb2dde9a9080d388841a6
ShinojSurendran <me.shinoj@gmail.com>
2025-12-08 11:20:11 +0530

    Assignment1_DataTypes

---
+++ src/language/basics/HelloWorld.java
@@ -0,0 +1,16 @@
+package language.basics;
+
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello, World"); //Sysout+control+space //Control+Click => to see the background code
+
+
+//System.out.println("Hello, World");
+
+	}
+
+}
